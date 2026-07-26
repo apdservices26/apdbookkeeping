1a79c694ed7e215561cb73d9b1dd6f1c9f8b566a
apdservices26 <apdservices26@gmail.com>
2026-07-26 00:22:14 -0600

    V10
    Version added, Invoice logo resolved

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "apd-bookkeeping-v9";
+const CACHE = "apd-bookkeeping-v10";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./favicon.png", "./icon-192.png", "./icon-512.png", "./logo_white.png"];
 
 self.addEventListener("install", (event) => {
